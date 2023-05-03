e3ad4dc274eb4d789994cf1a13c2a6fc59e111ad
ox161472 <ox161472@gmail.com>
2023-05-04 01:15:52 +0900

    연산자

---
+++ chap02/src/sec04/exam04/QstopExample.java
@@ -0,0 +1,20 @@
+package sec04.exam04;
+
+public class QstopExample {
+
+	public static void main(String[] args) throws Exception{
+		int keyCode;
+		
+		while(true) {
+			keyCode = System.in.read(); //class . .method
+			System.out.println("keyCode: " + keyCode);
+			if(keyCode == 113) {
+				break; //keyCode가 113과 동일한 경우 실행
+			}
+		}
+		
+		System.out.println("종료");
+	}
+
+}
+
